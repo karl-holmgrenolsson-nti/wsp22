5489a5fcb1378da4d92fec3625cd7a7ee46de3c6
karl-holmgrenolsson-nti <karl.holmgrenolsson@elev.ga.ntig.se>
2022-04-24 16:27:54 +0200

    test

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
-require 'sinatra'
-require 'slim'
-require 'sqlite3'
+require "sinatra"
+require "slim"
+require "sqlite3"
+
+get ("/") do
+  return "drinks"
+end
 
 get("/drinks/new") do
-    slim(:"drinks/new")
+  slim(:"drinks/new")
 end
